462b6794d51a2ec6986ca499c05cea0009ec58b0
onagova <naepanda@hotmail.com>
2020-01-27 10:39:11 +0700

    Landing Page Resume

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+function isOverflow(el) {
+  return el.scrollHeight > el.clientHeight || el.scrollWidth > el.clientWidth;
+}
+
+function fitText(el) {
+  let n = 0;
+
+  do {
+    $(el).css('font-size', ++n);
+  } while (!isOverflow(el));
+
+  $(el).css('font-size', --n);
+
+  console.log(el + ' fit at: ' + n + 'px');
+}
+
+/*window.onload = function() {
+  this.fitText(document.getElementById('fullname'));
+  this.fitText(document.getElementById('nickname'));
+}*/
